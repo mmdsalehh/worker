8a542a4d72bd0a14e6d502e70d8afc87d5fb29d1
mmd <mmdsalehh@gmail.com>
2024-11-29 16:18:19 +0330

    edit 5

--- src/protocols/vless/vlessOverWSHandler.ts
+++ src/protocols/vless/vlessOverWSHandler.ts
@@ -30,88 +30,68 @@ export async function vlessOverWSHandler(request: Request, env: Env) {
   let udpStreamWrite: ((chunk: Uint8Array) => void) | null = null;
   let isDns = false;
 
-  // ws --> remote
-  readableWebSocketStream
-    .pipeTo(
-      new WritableStream({
-        async write(chunk, controller) {
-          if (isDns && udpStreamWrite) {
-            return udpStreamWrite(chunk);
-          }
-          if (remoteSocketWapper.value) {
-            const writer = remoteSocketWapper.value.writable.getWriter();
-            await writer.write(chunk);
-            writer.releaseLock();
-            return;
-          }
+  readableWebSocketStream.pipeTo(
+    new WritableStream({
+      async write(chunk) {
+        if (isDns && udpStreamWrite) {
+          return udpStreamWrite(chunk);
+        }
+        if (remoteSocketWapper.value) {
+          const writer = remoteSocketWapper.value.writable.getWriter();
+          await writer.write(chunk);
+          writer.releaseLock();
+          return;
+        }
 
-          const {
-            hasError,
-            message,
-            portRemote = 443,
-            addressRemote = "",
-            rawDataIndex,
-            vlessVersion = new Uint8Array([0, 0]),
-            isUDP,
-          } = await processVlessHeader(chunk, userID);
-          address = addressRemote;
-          portWithRandomLog = `${portRemote}--${Math.random()} ${
-            isUDP ? "udp " : "tcp "
-          } `;
-          if (hasError) {
-            // controller.error(message);
-            throw new Error(message); // cf seems has bug, controller.error will not end stream
-            // webSocket.close(1000, message);
-            return;
-          }
-          // if UDP but port not DNS port, close it
-          if (isUDP) {
-            if (portRemote === 53) {
-              isDns = true;
-            } else {
-              // controller.error('UDP proxy only enable for DNS which is port 53');
-              throw new Error("UDP proxy only enable for DNS which is port 53"); // cf seems has bug, controller.error will not end stream
-              return;
-            }
-          }
-          // ["version", "附加信息长度 N"]
-          const vlessResponseHeader = new Uint8Array([vlessVersion[0], 0]);
-          const rawClientData = chunk.slice(rawDataIndex);
+        const {
+          isUDP,
+          message,
+          hasError,
+          rawDataIndex,
+          portRemote = 443,
+          addressRemote = "",
+          vlessVersion = new Uint8Array([0, 0]),
+        } = await processVlessHeader(chunk, userID);
+        address = addressRemote;
+        portWithRandomLog = `${portRemote}--${Math.random()} ${
+          isUDP ? "udp " : "tcp "
+        } `;
 
-          // TODO: support udp here when cf runtime has udp support
-          if (isDns) {
-            const { write } = await handleUDPOutBound(
-              webSocket,
-              vlessResponseHeader,
-              log
-            );
-            udpStreamWrite = write;
-            udpStreamWrite(rawClientData);
-            return;
-          }
+        if (hasError) throw new Error(message);
 
-          handleTCPOutBound(
-            request,
-            remoteSocketWapper,
-            addressRemote,
-            portRemote,
-            rawClientData,
+        if (isUDP) {
+          if (portRemote !== 53)
+            throw new Error("UDP proxy only enable for DNS which is port 53");
+
+          isDns = true;
+        }
+        const vlessResponseHeader = new Uint8Array([vlessVersion[0], 0]);
+        const rawClientData = chunk.slice(rawDataIndex);
+
+        if (isDns) {
+          const { write } = await handleUDPOutBound(
             webSocket,
             vlessResponseHeader,
             log
           );
-        },
-        close() {
-          log(`readableWebSocketStream is close`);
-        },
-        abort(reason) {
-          log(`readableWebSocketStream is abort`, JSON.stringify(reason));
-        },
-      })
-    )
-    .catch((err) => {
-      log("readableWebSocketStream pipeTo error", err);
-    });
+          udpStreamWrite = write;
+          udpStreamWrite(rawClientData);
+          return;
+        }
+
+        handleTCPOutBound(
+          request,
+          remoteSocketWapper,
+          addressRemote,
+          portRemote,
+          rawClientData,
+          webSocket,
+          vlessResponseHeader,
+          log
+        );
+      },
+    })
+  );
 
   return new Response(null, {
     status: 101,
