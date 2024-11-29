d47aaa95de6549389f8174dfea81fb127088408a
mmd <mmdsalehh@gmail.com>
2024-11-29 16:23:07 +0330

    edit 6

--- src/protocols/vless/vlessRemoteSocketToWS.ts
+++ src/protocols/vless/vlessRemoteSocketToWS.ts
@@ -9,12 +9,11 @@ export default async function vlessRemoteSocketToWS(
   log: (info: string, event?: string) => void
 ) {
   let vlessHeader = vlessResponseHeader;
-  let hasIncomingData = false; // check if remoteSocket has incoming data
+  let hasIncomingData = false;
+
   await remoteSocket.readable
     .pipeTo(
       new WritableStream({
-        start() {},
-
         async write(chunk: Uint8Array, controller) {
           hasIncomingData = true;
           if (webSocket.readyState !== WS_READY_STATE_OPEN) {
@@ -27,29 +26,9 @@ export default async function vlessRemoteSocketToWS(
             webSocket.send(chunk);
           }
         },
-        close() {
-          log(
-            `remoteConnection!.readable is close with hasIncomingData is ${hasIncomingData}`
-          );
-        },
-        abort(reason) {
-          console.error(`remoteConnection!.readable abort`, reason);
-        },
       })
     )
-    .catch((error) => {
-      console.error(
-        `vlessRemoteSocketToWS has exception `,
-        error.stack || error
-      );
-      safeCloseWebSocket(webSocket);
-    });
+    .catch(() => safeCloseWebSocket(webSocket));
 
-  // seems is cf connect socket have error,
-  // 1. Socket.closed will have error
-  // 2. Socket.readable will be close without any data coming
-  if (hasIncomingData === false && retry) {
-    log(`retry`);
-    retry();
-  }
+  if (hasIncomingData === false && retry) retry();
 }
