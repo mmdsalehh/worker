29fdef0e07e165ebeb4d7aa23e94bfcfdbb350ef
mmd <mmdsalehh@gmail.com>
2024-11-29 15:41:02 +0330

    edit 3

--- src/protocols/vless/makeReadableWebSocketStream.ts
+++ src/protocols/vless/makeReadableWebSocketStream.ts
@@ -10,30 +10,24 @@ export default function makeReadableWebSocketStream(
   const stream = new ReadableStream({
     start(controller) {
       webSocketServer.addEventListener("message", (event) => {
-        if (readableStreamCancel) {
-          return;
-        }
+        if (readableStreamCancel) return;
+
         const message = event.data;
         controller.enqueue(message);
       });
 
-      // The event means that the client closed the client -> server stream.
-      // However, the server -> client stream is still open until you call close() on the server side.
-      // The WebSocket protocol says that a separate close message must be sent in each direction to fully close the socket.
       webSocketServer.addEventListener("close", () => {
-        // client send close, need close server
-        // if stream is cancel, skip controller.close
         safeCloseWebSocket(webSocketServer);
-        if (readableStreamCancel) {
-          return;
-        }
+        if (readableStreamCancel) return;
+
         controller.close();
       });
+
       webSocketServer.addEventListener("error", (err) => {
         log("webSocketServer has error");
         controller.error(err);
       });
-      // for ws 0rtt
+
       const { earlyData, error } = base64ToArrayBuffer(earlyDataHeader);
       if (error) {
         controller.error(error);
@@ -41,17 +35,8 @@ export default function makeReadableWebSocketStream(
         controller.enqueue(earlyData);
       }
     },
-    pull(controller) {
-      // if ws can stop read if stream is full, we can implement backpressure
-      // https://streams.spec.whatwg.org/#example-rs-push-backpressure
-    },
     cancel(reason) {
-      // 1. pipe WritableStream has error, this cancel will called, so ws handle server close into here
-      // 2. if readableStream is cancel, all controller.close/enqueue need skip,
-      // 3. but from testing controller.error still work even if readableStream is cancel
-      if (readableStreamCancel) {
-        return;
-      }
+      if (readableStreamCancel) return;
       log(`ReadableStream was canceled, due to ${reason}`);
       readableStreamCancel = true;
       safeCloseWebSocket(webSocketServer);
